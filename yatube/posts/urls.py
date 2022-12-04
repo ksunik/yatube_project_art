ce3d5b585161727671720c9e4cc030e4667ceb19
ksunik <zhuravleva.ksenija@gmail.com>
2022-12-04 17:32:37 +0500

    Working vertion of project

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     # 'var_group_list.html' - это имя переменной в шаблоне DTL, 
     # 'group_list.html/' - значение переменной
     # Теперь все ссылки в DTL можно поменять тут одним махом
-    path('group_list.html/', views.group_list, name='var_group_list.html'),
-    path('group/<slug:slug>/', views.group_posts, name='group_slug')
+    # path('group_list.html/', views.group_list, name='var_group_list.html'),
+    path('group/<slug>/', views.group_posts, name='group_slug')
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -23,17 +23,18 @@ def index(request):
     return render(request, 'posts/index.html', context)
 
 
-def group_list(request):
-    template = 'posts/group_list.html'
-    context = {'text': 'Здесь будет информация о группах проекта Yatube'}
-    return render(request, template, context)
+# def group_list(request):
+#     template = 'posts/group_list.html'
+#     context = {'text': 'Здесь будет информация о группах проекта Yatube'}
+#     return render(request, template, context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'group': group,
-        'posts': posts,
+       'group': group,
+       'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
 
